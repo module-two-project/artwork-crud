5fbe3ca3c962ffbe86d8508e0a7dd1ee8202bd51
Saskia Peach <saskiapeach@gmail.com>
2022-10-09 12:22:53 +0100

    add route to view artwork details

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ app.use("/", index);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
-app.use("/", require("./routes/artwork.routes"))
+
+const artworkRoutes = require("./routes/artwork.routes");
+app.use("/", artworkRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/artwork.routes.js
+++ routes/artwork.routes.js
@@ -2,35 +2,84 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const Artwork = require("../models/Artwork.model");
 
-
 //CREATE: display form
-const router = require("express").Router()
-router.get("/artwork/create", (req, res,next) => {
-    res.render("artwork/artwork-create");
-})
+const router = require("express").Router();
+router.get("/artwork/create", (req, res, next) => {
+  res.render("artwork/artwork-create");
+});
 
-// CREATE: post process form 
+// CREATE: post process form
 
 router.post("/artwork/create", (req, res, next) => {
-  
-    const artworkDetails = {
-      title: req.body.title,
-      description: req.body.description,
-      artist: req.body.artist,
-      date: req.body.date,
-    }
-  
-    Artwork.create(artworkDetails)
-      .then(artworkDetails => {
-        res.redirect("/");
-      })
-      .catch(err => {
-        console.log("error creating new artwork in DB", err);
-        next(err);
-      })
-  
-  });
+  const artworkDetails = {
+    title: req.body.title,
+    description: req.body.description,
+    artist: req.body.artist,
+    date: req.body.date,
+  };
 
+  Artwork.create(artworkDetails)
+    .then((artworkDetails) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log("error creating new artwork in DB", err);
+      next(err);
+    });
+});
+
+//View all artwork
+
+router.get("/artwork", (req, res, next) => {
+  Artwork.find()
+    .then((artworkFromDB) => {
+      res.render("artwork/artwork-list", { artwork: artworkFromDB });
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
+});
+
+//Show Artwork Details
+router.get("/artwork/:artworkId", (req, res, next) => {
+  const artworkId = req.params.artworkId;
+
+  Artwork.findById(artworkId)
+    .then( artworkDetails => {
+      res.render("artwork/artwork-details", artworkDetails);
+    } )
+    .catch( err => {
+      console.log(err);
+      next();
+    })
+});
+
+
+
+//Update Artwork Info 
+router.get("/artwork/:artworkId/update", (req, res, next)=>{
+  Artwork.findById(req.params.artworkId)
+  .then((artworkDetails) => {
+    res.render("artwork/artwork-update", artworkDetails);
+  })
+  .catch(err => console.log(err))
+  next()
+})
+
+router.post("/artwork/:artworkId/update", (req, res, next)=> {
+  const artworkId = req.params.artworkId
+  const newDetails = {
+    title: req.body.title,
+    date: req.body.date,
+    description: req.body.description,
+    artist: req.body.artist
+  }
+  Artwork.findByIdAndUpdate(artworkId, newDetails)
+  .then(()=> {res.redirect(`/artwork/${artworkId}`)})
+  .catch(err => console.log(err))
+  next()
+})
 
 
 
