dadd7f2c0990d2deb90fa23ce8369afc5687d11a
Saskia Peach <saskiapeach@gmail.com>
2022-10-10 09:43:28 +0100

    add navbar to index

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const app = express();
 require("./config")(app);
 
 const capitalized = require("./utils/capitalized");
-const projectName = "project2";
+const projectName = "Artwork Gallery";
 
-app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalized(projectName)} created by Marc & Saskia`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index.routes");
